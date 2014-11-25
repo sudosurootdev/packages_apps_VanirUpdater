0503ee9a6a3139a99311a55e5ed12d1850e535c7
nuclearmistake <nuclearmistake@gmail.com>
2014-11-25 10:43:07 -0500

    Revert "FIX NPE ON "LARGE" SCREEN DEVICES"
    This reverts commit 65d65ee9a582f29b5db13d0b8a0dbc0819b1368b.

--- src/com/vanir/updater/UpdatesSettings.java
+++ src/com/vanir/updater/UpdatesSettings.java
@@ -152,11 +152,8 @@ public class UpdatesSettings extends PreferenceActivity implements
 
         // Set 'HomeAsUp' feature of the actionbar to fit better into Settings
         final ActionBar bar = getActionBar();
-        try {
-             bar.setDisplayHomeAsUpEnabled(true);
-        } catch(java.lang.NullPointerException ex) {
-             // YOLO
-        }
+	if (bar != null)
+	        bar.setDisplayHomeAsUpEnabled(true);
 
         // Turn on the Options Menu
         invalidateOptionsMenu();
