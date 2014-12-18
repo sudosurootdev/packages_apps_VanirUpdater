ac7d49edc51c0a210f128254fd0c63667f36c215
MartinRo <martin@rodemann.org>
2014-12-18 15:49:43 +0100

    VanirUpdater: Materialize
    This commit is based on "CMUpdater : Materialize" from Michael Bestas
    Original Commit:
    http://review.cyanogenmod.org/#/c/80305/
    
    PS2: typo
    PS3: Whitespaces and tabs
    PS4: more tabs
    
    Change-Id: Iabb5a1aca5e650e2406e95f52c741dfd9a9be962

--- src/com/vanir/updater/utils/Utils.java
+++ src/com/vanir/updater/utils/Utils.java
@@ -26,8 +26,11 @@ import android.os.UserHandle;
 import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
 import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 import android.util.Log;
 
+import android.view.DisplayInfo;
+import android.view.WindowManager;
 import com.vanir.updater.R;
 import com.vanir.updater.misc.Constants;
 import com.vanir.updater.service.UpdateCheckService;
@@ -44,6 +47,14 @@ import java.net.MalformedURLException;
 import java.util.LinkedList;
 
 public class Utils {
+    // Device type reference
+    private static int sDeviceType = -1;
+
+    // Device types
+    private static final int DEVICE_PHONE = 0;
+    private static final int DEVICE_HYBRID = 1;
+    private static final int DEVICE_TABLET = 2;
+
     private Utils() {
         // this class is not supposed to be instantiated
     }
@@ -221,5 +232,38 @@ public class Utils {
         } catch (IOException e) {
         }
         return ret;
+    }
+    private static int getScreenType(Context context) {
+        if (sDeviceType == -1) {
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            DisplayInfo outDisplayInfo = new DisplayInfo();
+            wm.getDefaultDisplay().getDisplayInfo(outDisplayInfo);
+            int shortSize = Math.min(outDisplayInfo.logicalHeight, outDisplayInfo.logicalWidth);
+            int shortSizeDp = shortSize * DisplayMetrics.DENSITY_DEFAULT
+                    / outDisplayInfo.logicalDensityDpi;
+            if (shortSizeDp < 600) {
+                // 0-599dp: "phone" UI with a separate status & navigation bar
+                sDeviceType =  DEVICE_PHONE;
+            } else if (shortSizeDp < 720) {
+                // 600-719dp: "phone" UI with modifications for larger screens
+                sDeviceType = DEVICE_HYBRID;
+            } else {
+                // 720dp: "tablet" UI with a single combined status & navigation bar
+                sDeviceType = DEVICE_TABLET;
+            }
+        }
+        return sDeviceType;
+    }
+
+    public static boolean isPhone(Context context) {
+        return getScreenType(context) == DEVICE_PHONE;
+    }
+
+    public static boolean isHybrid(Context context) {
+        return getScreenType(context) == DEVICE_HYBRID;
+    }
+
+    public static boolean isTablet(Context context) {
+        return getScreenType(context) == DEVICE_TABLET;
     }
 }
