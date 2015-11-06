5e07d6343ecdb69560078c701f34a2e88d08de2b
nuclearmistake <nuclearmistake@gmail.com>
2015-11-06 07:55:23 -0500

    remove unused, broken imports
    Change-Id: I12841637468285974ae1466e125d552ca4ecaf20

--- src/com/vanir/updater/service/UpdateCheckService.java
+++ src/com/vanir/updater/service/UpdateCheckService.java
@@ -30,20 +30,6 @@ import com.vanir.updater.misc.UpdateInfo;
 import com.vanir.updater.receiver.DownloadReceiver;
 import com.vanir.updater.utils.Utils;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
